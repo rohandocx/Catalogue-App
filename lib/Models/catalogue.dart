b3825b08be93d3e879d5769a6c64f60d207aebff
rohandocx <rohan2056.be20@chitkara.edu.in>
2022-06-21 17:25:19 +0530

    Day 9 completed

---
+++ lib/Models/catalogue.dart
@@ -0,0 +1,23 @@
+class Item {
+  final String id;
+  final String name;
+  final String description;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.description, this.price, this.color, this.image});
+}
+
+final Products = [Item(
+   
+   id: "001",
+   name: "iphone 12  pro ",
+   description: " Apple 12th Generation 128 gb",
+   price: 999,
+   color: "#33505a",
+   image: "assets/images/iphone_12.jpg",
+
+
+
+)];
